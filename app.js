fb166aa59074cf6e4bb320553436ba4300b39da6
unknown <mihirraj2119@gmail.com>
2022-07-19 22:26:22 +0530

    Initial commit

---
+++ app.js
@@ -0,0 +1,110 @@
+var mysong1 = new Audio();
+mysong1.src = 'audio/1.mp3';
+
+const playsong1 = () =>{
+	mysong1.play();
+}
+
+const pausesong1 = () =>{
+	mysong1.pause();
+}
+var mysong2 = new Audio();
+mysong2.src = 'audio/2.mp3';
+
+const playsong2 = () =>{
+	mysong2.play();
+}
+
+const pausesong2 = () =>{
+	mysong2.pause();
+}
+var mysong3 = new Audio();
+mysong3.src = 'audio/3.mp3';
+
+const playsong3 = () =>{
+	mysong3.play();
+}
+
+const pausesong3 = () =>{
+	mysong3.pause();
+}
+var mysong4 = new Audio();
+mysong4.src = 'audio/4.mp3';
+
+const playsong4 = () =>{
+	mysong4.play();
+}
+
+const pausesong4 = () =>{
+	mysong4.pause();
+}
+var mysong5 = new Audio();
+mysong5.src = 'audio/5.mp3';
+
+const playsong5 = () =>{
+	mysong5.play();
+}
+
+const pausesong5 = () =>{
+	mysong5.pause();
+}
+var mysong6 = new Audio();
+mysong6.src = 'audio/6.mp3';
+
+const playsong6 = () =>{
+	mysong6.play();
+}
+
+const pausesong6 = () =>{
+	mysong6.pause();
+}
+var mysong7 = new Audio();
+mysong7.src = 'audio/7.mp3';
+
+const playsong7 = () =>{
+	mysong7.play();
+}
+
+const pausesong7 = () =>{
+	mysong7.pause();
+}
+var mysong8 = new Audio();
+mysong8.src = 'audio/8.mp3';
+
+const playsong8 = () =>{
+	mysong8.play();
+}
+
+const pausesong8 = () =>{
+	mysong8.pause();
+}
+var mysong9 = new Audio();
+mysong9.src = 'audio/9.mp3';
+
+const playsong9 = () =>{
+	mysong9.play();
+}
+
+const pausesong9 = () =>{
+	mysong9.pause();
+}
+var mysong10 = new Audio();
+mysong10.src = 'audio/10.mp3';
+
+const playsong10 = () =>{
+	mysong10.play();
+}
+
+const pausesong10 = () =>{
+	mysong10.pause();
+}
+
+/*<script>
+      function play() {
+        var audio = document.getElementById("audio");
+        audio.play();
+      }
+    </script>
+
+    <input type="button" value="PLAY" onclick="play()">
+    <audio id="audio" src="1.mp3"></audio>*/
